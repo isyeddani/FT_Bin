4e7550e2f6f191e972a6db978c5a9cdbc391040d
Syed Daniyal Ahmed <syeddani23.99@gmail.com>
2025-08-08 16:51:36 +0500

    Bat file change

--- PushBinFile.bat
+++ PushBinFile.bat
@@ -2,12 +2,23 @@
 :: Copy firmware.bin to the same folder as this .bat file
 copy "D:\Daniyal Stingray\FuelDispenser\Firmwares\FuelTrack\.pio\build\esp32-s3-devkitc-1-n16r8v\firmware.bin" "%~dp0firmware.bin"
 
-:: Prompt for commit message
-set /p commit_message="Enter commit message: "
+:: Check if there are any changes to commit
+git diff --quiet
+if %errorlevel% neq 0 (
+    :: If there are changes, prompt for commit message
+    :prompt_message
+    set /p commit_message="Enter commit message: "
+    if "%commit_message%"=="" (
+        echo Commit message cannot be empty. Please enter a valid message.
+        goto prompt_message
+    )
 
-:: Git commit and push
-git add .
-git commit -m "%commit_message%"
-git push
+    :: Git commit and push
+    git add .
+    git commit -m "%commit_message%"
+    git push
+) else (
+    echo No changes to commit.
+)
 
 pause
